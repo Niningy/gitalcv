8cb4f9ba9fc1db959a5c4e996c7b4a4ee4a8a1c5
Niningy <116931859+Niningy@users.noreply.github.com>
2023-03-07 15:31:23 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import numpy as np
+import PIL
+
+# General Setting
+st.header('Digital CV / Nining Yuningsih')
+st.image('profile-pic.png', width = 150)
+st.file_uploader('CV.pdf')
+
+    
+st.write('''
+Administrasion, Customerservices, sales outlate, administrasion_hr,administrasion receptionits,sales,administrasion operational, digital sales"
+''')
+st.write('niningy383@gmail.com')
+st.write('Phone:+6285817253707')
+st.write('Whatsapp: wa.me/+6285817253707')
+st.write('''
+     Linkedin':'https://Linkedin.com',
+    'Github':'https://github.com',
+    'tWitter':'https://twitter.com',
+    ''')
+
+st.header('ADUCATION')
+st.write('Universitas Tebuka(UT) Bachelor of Accounting 2018-2023')
+st.write('High School SMA PANDU IPA Passed 2011')
+st.write('MTS TSANAWIAH Passed 2008')
+st.write('MI SIRAJUL HUDA Passed 2007')
+
+st.header('NON FORMAL EDUCATION')
+st.write('1. Training Course Computer High School')
+st.write('2. Chief of Kadiv.Progrram Seminar Education Youth Meeting')
+st.write('3. English Club')
+st.write('4. TOEFL')
+st.write('5. Training Course online SAP MM and SAP Netweaver ABAP Program')
+st.write('6.Training Course Python Progrraming')
+
+
+st.header('JOB EXPERIENCES')
+st.write('1.Briton School Of English (BSE) Course and Tranlation Bording Position Administration - South Jakarta 2012-2013')
+st.write('2.PT.Garuda Ekspress Delivary (Ekspedition Company) Customer services & Sales Outlate Position South Jakarta 2013-2014')
+st.write('3.PT.Carefastindo Last Position Administrasi Support Logistic West Jakarta 2014-2020')
+st.write('4.PT.HIJAU BERDIKARI INDONESIA last Position Administrasi Opeerational')
+st.write('5.Founder Wijayakusumasport online shop umkm Bogor West Java 2020-2021')
+st.write('6.Sales\Marketing Online 2021-Now')
+st.write('7.freelancer Tutor in the PKBM Bording School 2022-Now')
+
+st.header('Certificates & Awards')
+st.write('1.Certificate Computer program(Ms.Word,Ms.Ecxel,Ms.Power Point)')
+st.write('2.Certificate TOEFL of Eligmu Course')
+st.write('3.Certificate Royalti Award 2019 of PT.Carefastindo & Congratulation Achieve Business of PT.Carefastindo')
+st.write('4.Certificate SAP MM of BelajarSAP course')
+st.write('5.Certificate Auditor 3 SKP of Auditor Indonesia')
+st.write('6.Certificate networking Academy(Cybersecurity and Cyber Esentials"CISO")')
+st.write('7. Certificate Akuntansi 2 SKP of open University indonesia(Universitas Terbuka(UT))')
+st.write('8.Certificate call paper of the P3EI(Pusat Pengkajian Dan Pengembangan Ekonomi Islam)')
+st.write('9.Certificate training UMKM class start up Biz-UMKM(Usaha Kecil Menengah)')
+
+
+
